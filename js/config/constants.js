89351554bc49f54275e48c69413b5dbb3c70c033
liugonghua <2916400875@qq.com>
2026-04-07 16:54:43 +0800

    feat: add skillsmp.com to default bookmarks

--- js/config/constants.js
+++ js/config/constants.js
@@ -2,6 +2,7 @@ export const defaultBookmarks = [
   { title: "Grok", url: "https://grok.com", icon: "https://www.google.com/s2/favicons?domain=grok.com&sz=128" },
   { title: "ChatGPT", url: "https://chatgpt.com", icon: "https://www.google.com/s2/favicons?domain=chatgpt.com&sz=128" },
   { title: "X", url: "https://x.com", icon: "https://www.google.com/s2/favicons?domain=x.com&sz=128" },
+  { title: "Skills", url: "https://skillsmp.com", icon: "https://www.google.com/s2/favicons?domain=skillsmp.com&sz=128" },
   { title: "百度翻译", url: "https://fanyi.baidu.com", icon: "https://www.google.com/s2/favicons?domain=fanyi.baidu.com&sz=128" },
   { title: "GitHub", url: "https://github.com", icon: "https://www.google.com/s2/favicons?domain=github.com&sz=128" }
 ];
